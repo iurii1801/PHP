5e9ae68c7ec605ec9ec79c657d40ce2757eb73da
Iurii Bogdanov <145650269+iurii1801@users.noreply.github.com>
2025-02-12 19:57:03 +0200

    Add files via upload

---
+++ lab1_Bogdanov_Iurii_I2302/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "Привет, мир! <br>" ;
+
+echo "Hello, World with echo! <br>"; 
+print "Hello, World with print!<br>";
+
+$days = 288; 
+$message = "Все возвращаются на работу!";
+
+echo "Дней: $days .... Сообщение: $message <br>";
+echo "Дней: $days .... Сообщение: $message <br>";
+
